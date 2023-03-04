8bda495c73dee603c6dfe30e534729fd246416d9
MR.R <ivanryabovg@gmail.com>
2023-03-04 22:39:05 +0300

    Add condition if-else

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "иван")
+{
+    Console.WriteLine("Привет классный Иван!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
