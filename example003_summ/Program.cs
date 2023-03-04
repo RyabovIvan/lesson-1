c8e43661b8c3441aef8c96f353eca7a5ab2656a2
MR.R <ivanryabovg@gmail.com>
2023-03-04 22:11:03 +0300

    Add Random ex3

--- example003_summ/Program.cs
+++ example003_summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1, 11);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 11);
+Console.WriteLine(numberB);
 int summ = numberA + numberB;
 Console.WriteLine(summ);
